8eafebf2ec69752cab9f3c74677593d794708e4f
Jason Ekstrand <jason@jlekstrand.net>
2013-04-12 09:35:46 -0500

    Add basic "Finger" classes to the TouchHandler

--- src/net/jlekstrand/wheatley/TouchHandler.java
+++ src/net/jlekstrand/wheatley/TouchHandler.java
@@ -1,5 +1,8 @@
 package net.jlekstrand.wheatley;
 
+import java.util.HashMap;
+
+import org.freedesktop.wayland.Fixed;
 import org.freedesktop.wayland.server.Resource;
 import org.freedesktop.wayland.server.Client;
 
@@ -7,7 +10,40 @@ import org.freedesktop.wayland.protocol.wl_touch;
 
 public class TouchHandler
 {
+    private class Finger
+    {
+        public final int id;
+
+        private Surface surface;
+        private Resource resource;
+
+        public Finger(int id)
+        {
+            this.id = id;
+        }
+
+        public void handleDown(int serial, int time, Fixed x, Fixed y)
+        {
+            // surface = shell.getSurfaceAt(x.asInt(), y.asInt());
+            // resource = touchResources.get(surface.resource.getClient());
+
+            wl_touch.postDown(resource, serial, time,
+                    surface.resource, id, x, y);
+        }
+
+        public void handleMotion(int time, Fixed x, Fixed y)
+        {
+            wl_touch.postMotion(resource, time, id, x, y);
+        }
+
+        public void handleUp(int serial, int time)
+        {
+            wl_touch.postUp(resource, serial, time, id);
+        }
+    }
+
     final ClientResourceMap resources;
+    private HashMap<Integer, Finger> fingers;
 
     public TouchHandler()
     {
@@ -18,6 +54,29 @@ public class TouchHandler
     {
         resources.addResource(client.addObject(
                 wl_touch.WAYLAND_INTERFACE, id, null));
+    }
+
+    void handleDown(int serial, int time, int id, Fixed x, Fixed y)
+    {
+        fingers.get(id).handleDown(serial, time, x, y);
+    }
+
+    void handleMotion(int time, int id, Fixed x, Fixed y)
+    {
+        fingers.get(id).handleMotion(time, x, y);
+    }
+
+    void handleUp(int serial, int time, int id)
+    {
+        fingers.get(id).handleUp(serial, time);
+    }
+
+    void handleFrame()
+    {
+    }
+
+    void handleCancel()
+    {
     }
 }
 
